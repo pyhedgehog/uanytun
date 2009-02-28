11fe9b5a22d0d09dcdc781f17553b35c6603acab
Christian Pointner <equinox@anytun.org>
2009-02-28 14:58:44 +0000

    fixed prefix length at example post-up scripts

--- etc/uanytun/client1/post-up.sh
+++ etc/uanytun/client1/post-up.sh
@@ -3,7 +3,7 @@
 ip link set dev $1 up
 ip link set dev $1 mtu 1400
 ip addr add dev $1 192.168.123.1/24
-ip addr add dev $1 fec0::1/128
+ip addr add dev $1 fec0::1/64
 
 # disable ICMP redirects as they don't work within the tunnel
 echo 0 > /proc/sys/net/ipv4/conf/$1/send_redirects
