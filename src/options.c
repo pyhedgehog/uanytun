38a29f523a0c2536c05ca4056b365f6b0aee81da
Christian Pointner <equinox@anytun.org>
2017-01-04 21:56:35 +0100

    cherry-pick build reproducibility fixes from master

--- src/options.c
+++ src/options.c
@@ -480,13 +480,13 @@ void options_print_usage()
 
 void options_print_version()
 {
-  printf("%s\n", VERSION_STRING_0);
+  printf("%s\n", VERSION_STRING);
 #if defined(__clang__)
-  printf("%s, using CLANG %s\n", VERSION_STRING_1, __clang_version__);
+  printf("built using CLANG %s with %s crypto library.\n", __clang_version__, CRYPTO_LIB_NAME);
 #elif defined(__GNUC__)
-  printf("%s, using GCC %d.%d.%d\n", VERSION_STRING_1, __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+  printf("built using GCC %d.%d.%d with %s crypto library\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__, CRYPTO_LIB_NAME);
 #else
-  printf("%s\n", VERSION_STRING_1);
+  printf("built using an unknown compiler with %s crypto library\n", CRYPTO_LIB_NAME, CRYPTO_LIB_NAME);
 #endif
 }
 
